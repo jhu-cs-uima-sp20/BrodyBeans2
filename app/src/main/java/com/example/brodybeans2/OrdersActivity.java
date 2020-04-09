5fbbdfb37c9e9f21ea7092938121099d65de7536
Justin Canedy <jcanedy1@jhu.edu>
2020-04-09 02:41:22 -0400

    Beginning of OrderActivities

--- app/src/main/java/com/example/brodybeans2/OrdersActivity.java
+++ app/src/main/java/com/example/brodybeans2/OrdersActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -14,13 +15,27 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class OrdersActivity extends AppCompatActivity {
 
-
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mOrdersDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private String email;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
 
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mOrdersDatabaseReference = mFirebaseDatabase.getReference().child("orders");
+
+        email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
 
     }
 }
