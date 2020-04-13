909f5d5c8acba6d15ac00ce4e2a65a297ca1b276
Abigail Rehmet <arehmet1@jhu.edu>
2020-04-12 23:50:44 -0400

    added Settings button to toolbar and created corresponding xml and java files

---
+++ app/src/main/java/com/example/brodybeans2/SettingsActivity.java
@@ -0,0 +1,9 @@
+package com.example.brodybeans2;
+
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+}
