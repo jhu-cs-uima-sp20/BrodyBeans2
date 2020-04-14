13d479245aa61475aee7601f601d8de262f143c1
Hannah Lynn <hannah.k.lynn@gmail.com>
2020-04-14 17:28:33 -0500

    Edited java class for an item gave it a size, temperature and modification

--- app/src/main/java/com/example/brodybeans2/OrderItem.java
+++ app/src/main/java/com/example/brodybeans2/OrderItem.java
@@ -2,12 +2,26 @@ package com.example.brodybeans2;
 
 public class OrderItem {
     private String category;
+    private String temperature;
+    private String modifications;
+    private String size;
+
+
 
     public OrderItem() {}
 
     public OrderItem(String item) {
         category = item;
     }
+
+    public OrderItem(String item, String temp, String sz, String mod) {
+        category = item;
+        temperature = temp;
+        size = sz;
+        modifications = mod;
+    }
+
+
 
     public String getCategory() {
         return category;
@@ -16,5 +30,22 @@ public class OrderItem {
     public void setCategory(String category) {
         this.category = category;
     }
-}
 
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String sz) {
+        this.size = sz;
+    }
+    
+    public String getMod() {return this.modifications;}
+
+    public void setMod(String mod) { this.modifications = mod; }
+
+    public String getTemp() { return temperature;}
+
+    public void setTemp(String temp) { this.temperature = temp; }
+
+}
